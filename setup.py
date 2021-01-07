fea74398b9dcb6a4ab859a1d1028f8b75f088cfc
Alan Chin <akchin@us.ibm.com>
2021-01-06 16:08:33 -0800

    Release v0.0.4

--- setup.py
+++ setup.py
@@ -61,6 +61,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/elyra-ai/airflow-notebook',
-    version='0.0.3',
+    version='0.0.4',
     zip_safe=False,
 )
